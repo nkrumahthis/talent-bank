f7f617618cee1ff2034a298f4f3300d4984dc2d7
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-10-04 10:45:06 +0000

    install query-string

--- front/lib/utils.ts
+++ front/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,4 +14,24 @@ export function formatAmount(amount: number): string {
   });
 
   return formatter.format(amount);
+}
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string;
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true },
+  );
 }
