942ef790035c4bf34cbd537565c86abd33bc7faa
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-10-04 10:33:36 +0000

    front: headerbox

---
+++ front/types/index.d.ts
@@ -0,0 +1,12 @@
+declare interface HeaderBoxProps {
+  type?: "title" | "greeting";
+  title: string;
+  subtext: string;
+  user?: string;
+}
+
+declare interface TotalBalanceBoxProps {
+  accounts: Account[];
+  totalBanks: number;
+  totalCurrentBalance: number;
+}
